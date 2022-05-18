1d8d7abfb0665da4bbab881918040a604c7c7e99
Morgan DAYNES <morgan.daynes@solidnames.fr>
2022-05-18 09:36:31 +0000

    Feat: Adding login action and home redirect

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, ToastAndroid, TouchableOpacity, View } from 'react-native';
 import Background from '../components/Background';
 import Button from '../components/Button';
 import Input from '../components/Input';
@@ -6,18 +6,27 @@ import Styles from '../constants/Styles';
 import { Props } from '../utils/Props';
 
 export default function Login({ navigation }: Props<'Login'>) {
+  let user = "";
+  let pass = "";
+  const verifyLogin = () => {
+    if (user == "admin" && pass == "admin") {
+      navigation.navigate('Home');
+    } else {
+      ToastAndroid.show("Wrong username or password", ToastAndroid.LONG);
+    }
+  }
   return (
     <View style={Styles.root}>
         <Background></Background>
-        <View style={[Styles.container, styles.titleView]}><Text style={{color: "#fff", fontSize: 40}}>Login</Text></View>
+        <View style={[Styles.container, styles.titleView]}><Text style={{color: "#fff", fontSize: 40, fontWeight: "bold"}}>Login</Text></View>
         
-        <View style={[Styles.container]}>
-          <Input style={styles.inputs} placeholder='Email'></Input>
-          <Input style={styles.inputs} placeholder='Password' password={true}></Input>
+        <View style={[Styles.container, styles.input]}>
+          <Input style={styles.inputs} placeholder='Email' onChange={(e) => user = e}></Input>
+          <Input style={styles.inputs} placeholder='Password' password={true} onChange={(e) => pass = e}></Input>
         </View>
         
         <View style={[Styles.container, styles.button]}>
-          <Button text='Login'></Button>
+          <Button text='Login' onClick={verifyLogin}></Button>
         </View>
     </View>
   );
@@ -35,6 +44,10 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: Dimensions.get('window').height/1.1,
+    top: 0
+  },
+  input: {
+    marginTop: (Dimensions.get('window').height/2)-55,
     top: 0
   },
   inputs: {
--- utils/Props.ts
+++ utils/Props.ts
@@ -2,5 +2,6 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 type RootStackParamList = {
     Login: undefined;
+    Home: undefined;
   };
 export type Props<Screen extends keyof RootStackParamList> = NativeStackScreenProps<RootStackParamList, Screen, 'ONIPanel'>;
